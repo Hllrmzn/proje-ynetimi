1250a5abacf0b3f30a7d937c5727d4dd51c24044
Hllrmzn <97919174+Hllrmzn@users.noreply.github.com>
2023-06-21 10:49:22 +0300

    Add files via upload

---
+++ melanoma.py
@@ -0,0 +1,34 @@
+import os
+import torch
+import albumentations
+
+import numpy as np
+import pandas as pd
+
+import torch.nn as nn
+from sklearn import metrics
+from sklearn import model_selection
+from torch.nn import functional as F
+
+from wtfml.utils import EarlyStopping
+from wtfml.engine import Engine
+from wtfml.data_loaders.image import segmentation
+
+import pretrainedmodels
+
+
+class SEResnext50_32x4d(nn.Module):
+    def __init__(self, pretrained='imagenet'):
+        super(SEResnext50_32x4d, self).__init__()
+        
+        self.base_model = pretrainedmodels.__dict__[
+            "se_resnext50_32x4d"
+        ](pretrained=None)
+        if pretrained is not None:
+            self.base_model.load_state_dict(
+                torch.load(
+                    "../input/pretrained-model-weights-pytorch/se_resnext50_32x4d-a260b3a4.pth"
+                )
+            )
+
+        self.l0 = nn.Linear(2048, 1)
